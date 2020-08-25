7042eda07c8a1d213c95a3e9c6264595e23a6593
Gustavo Lima <gustavosnl@gmail.com>
2020-08-25 12:05:27 -0300

    changes usecase interface signature to return pagedData

--- domain/src/main/java/com/glima/domain/business/repository/BreedsRepository.kt
+++ domain/src/main/java/com/glima/domain/business/repository/BreedsRepository.kt
@@ -1,9 +1,10 @@
 package com.glima.domain.business.repository
 
+import androidx.paging.PagingData
 import com.glima.domain.business.model.Breed
 import kotlinx.coroutines.flow.Flow
 
 interface BreedsRepository {
 
-    suspend fun fetchBreedsByPage(page: Int): List<Breed>
+    suspend fun fetchBreedsByPage(page: Int): Flow<PagingData<Breed>>
 }
--- domain/src/main/java/com/glima/domain/business/usecase/FetchBreedsUseCase.kt
+++ domain/src/main/java/com/glima/domain/business/usecase/FetchBreedsUseCase.kt
@@ -1,14 +1,14 @@
 package com.glima.domain.business.usecase
 
+import androidx.paging.PagingData
 import com.glima.domain.business.model.Breed
 import com.glima.domain.business.repository.BreedsRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
 
-interface FetchBreedsUseCase : UseCase<Int, Breed>
+interface FetchBreedsUseCase : UseCase<Int, PagingData<Breed>>
 
 class FetchBreedsUseCaseImpl(private val repository: BreedsRepository) : FetchBreedsUseCase {
-    override suspend fun execute(params: Int): Flow<Breed> {
-        return repository.fetchBreedsByPage(params).asFlow()
+    override suspend fun execute(params: Int): Flow<PagingData<Breed>> {
+        return repository.fetchBreedsByPage(params)
     }
 }
