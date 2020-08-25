0452368ed4f38bcf1b86bb3e6a1c197ae94a1280
Gustavo Lima <gustavosnl@gmail.com>
2020-08-24 23:12:38 -0300

    adding lifecycle extensions

--- app/src/main/java/com/glima/ilovecats/BreedListFragment.kt
+++ app/src/main/java/com/glima/ilovecats/BreedListFragment.kt
@@ -1,22 +1,35 @@
 package com.glima.ilovecats
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.glima.domain.business.usecase.FetchBreedsUseCase
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    val usecase: FetchBreedsUseCase by inject()
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
+
+        lifecycleScope.launch {
+            val results = usecase.execute(1)
+            results
+        }
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
