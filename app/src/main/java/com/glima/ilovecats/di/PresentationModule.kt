272db7698de84eb69cdb8c62a6a73e926369a966
Gustavo Lima <gustavosnl@gmail.com>
2020-08-26 00:17:26 -0300

    first version of BreedDetailViewModel

--- app/src/main/java/com/glima/ilovecats/di/PresentationModule.kt
+++ app/src/main/java/com/glima/ilovecats/di/PresentationModule.kt
@@ -1,5 +1,7 @@
 package com.glima.ilovecats.di
 
+import com.glima.ilovecats.detail.BreedDetailViewModel
+import com.glima.ilovecats.detail.BreedDetailsFragmentArgs
 import com.glima.ilovecats.list.BreedListViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -10,5 +12,10 @@ object PresentationModule {
         viewModel {
             BreedListViewModel(get())
         }
+
+        viewModel { (args: BreedDetailsFragmentArgs) ->
+            BreedDetailViewModel(get(), args)
+        }
+
     }
 }
